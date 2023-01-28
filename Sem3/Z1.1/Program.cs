c11ca07af4dc253e2880868b8bba3f9264a2aa52
Olga <olgaingvar@gmail.com>
2023-01-28 12:41:33 +0200

    ...

---
+++ Sem3/Z1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+// Не использовать строки
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> даn
+System.Console.WriteLine("Ведите число");
+int reverseDigits(int num);
+    rev = 0;
+    while (num > 0) {
+        rev = rev * 10 + num % 10;
+        num = num / 10;}
+    return rev;
+
+System.Console.WriteLine($"{rev} Полиндром");
---
+++ Sem3/Z1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+// Не использовать строки
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+System.Console.WriteLine("Ведите число");
+string number = Console.ReadLine();
+int len = number.Length;
+if (len == 5)
+{
+    if (number [0] == number [4] && number [1] == number [3])
+    {
+        System.Console.WriteLine($"{number} Полиндром");
+    }
+    else
+    {
+        System.Console.WriteLine($"{number} Не полиндром");
+    }
+}
+else
+{
+System.Console.WriteLine($"{number} Число не пятизначное");
+}
